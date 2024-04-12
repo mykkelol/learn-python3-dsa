cab9721d39bf84bc6099a1de033aa68eed9d4b96
mykkelol <1micheal.sihavong2@gmail.com>
2024-04-11 22:14:08 -0700

    complete exercise

--- 03-linked-list/exercise.py
+++ 03-linked-list/exercise.py
@@ -73,8 +73,44 @@ class LinkedList:
         return temp
     
     def insert(self, index, value):
-        
-        
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            self.prepend(value)
+        if index == self.length - 1:
+            self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            self.pop_first()
+        if index == self.length - 1:
+            self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
     
     def print_list(self):
         temp = self.head

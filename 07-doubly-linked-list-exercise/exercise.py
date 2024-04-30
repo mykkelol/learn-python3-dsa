94502fc0bcb2728c9917364bec45b6ca6714ea04
mykkelol <1micheal.sihavong2@gmail.com>
2024-04-29 19:52:38 -0700

    add dll pop

--- 07-doubly-linked-list-exercise/exercise.py
+++ 07-doubly-linked-list-exercise/exercise.py
@@ -22,3 +22,17 @@ class DoublyLinkedList:
             self.tail = new_node
         self.length += 1
         return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp

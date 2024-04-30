4d171e9cc9eedae6d6109dc30c8113e0196945e6
mykkelol <1micheal.sihavong2@gmail.com>
2024-04-29 19:40:47 -0700

    add dll append

--- 07-doubly-linked-list-exercise/exercise.py
+++ 07-doubly-linked-list-exercise/exercise.py
@@ -10,3 +10,15 @@ class DoublyLinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True

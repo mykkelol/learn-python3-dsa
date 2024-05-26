d1b29aba3ae0d37c21da76aab86a533454527fb3
mykkelol <1micheal.sihavong2@gmail.com>
2024-05-26 12:14:51 -0700

    add stack push

--- 10-stacks-and-queues-exercise/exercise.py
+++ 10-stacks-and-queues-exercise/exercise.py
@@ -3,13 +3,23 @@ class Node:
         self.value = value
         self.next = None
 
-class LinkedList:
+class Stack:
     def __init__(self, value):
         new_node = Node(value)
         self.top = new_node
         self.height = 1
 
-    def print_list(self):
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+        return True
+
+    def print_stack(self):
         temp = self.top
         while temp:
             print(temp.value)

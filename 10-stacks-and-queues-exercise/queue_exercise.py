02f8f03b870e76d2f4ca40ca27efefdba470fc01
mykkelol <1micheal.sihavong2@gmail.com>
2024-06-01 10:52:35 -0700

    add dequeue

--- 10-stacks-and-queues-exercise/queue_exercise.py
+++ 10-stacks-and-queues-exercise/queue_exercise.py
@@ -20,6 +20,19 @@ class Queue:
             self.last = new_node
         self.length += 1
         return True
+    
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
 
     def print_queue(self):
         temp = self.first

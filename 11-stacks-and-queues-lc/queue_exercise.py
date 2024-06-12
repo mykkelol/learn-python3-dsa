cd9302ace7bdb42bb8139a26755a7da62681bf9f
mykkelol <1micheal.sihavong2@gmail.com>
2024-06-11 23:11:46 -0700

    add queue lc enqueue

---
+++ 11-stacks-and-queues-lc/queue_exercise.py
@@ -0,0 +1,17 @@
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+        
+    def enqueue(self, value):
+        while len(self.stack1) > 0:
+            self.stack2.append(self.stack1.pop())
+        self.stack1.append(value)
+        while len(self.stack2) > 0:
+            self.stack1.append(self.stack2.pop())
+
+    def peek(self):
+        return self.stack1[-1]
+
+    def is_empty(self):
+        return len(self.stack1) == 0

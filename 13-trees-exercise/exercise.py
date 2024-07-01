ace7e65ede9ed105ef678619db71a60af6350c87
mykkelol <1micheal.sihavong2@gmail.com>
2024-07-01 14:03:30 -0700

    add bst insert exercise

--- 13-trees-exercise/exercise.py
+++ 13-trees-exercise/exercise.py
@@ -8,6 +8,25 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if value == temp.value:
+                return False
+            if value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            if value > temp.value:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
 
 my_tree = BinarySearchTree()
 print(my_tree.root)

e3e04ea34ceca0bf89cc6ba6f5ab62518a2d2b03
mykkelol <1micheal.sihavong2@gmail.com>
2024-08-01 20:01:00 -0700

    add subarray sum

--- 15-hash-tables-exercise/exercise.py
+++ 15-hash-tables-exercise/exercise.py
@@ -92,3 +92,13 @@ def two_sum(nums, target):
             return [nums_map[compliment], i]
         nums_map[num] = i
     return []
+
+def subarray_sum(nums, target):
+    sum_index = {0: -1}
+    current_sum = 0
+    for i, num in enumerate(nums):
+        current_sum += num
+        if current_sum - target in sum_index:
+            return [sum_index[current_sum - target] + 1, i]
+        sum_index[current_sum] = i
+    return []

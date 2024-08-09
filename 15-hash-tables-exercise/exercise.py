104bf429571652a1364314c08ccf901b748f0ab6
mykkelol <1micheal.sihavong2@gmail.com>
2024-08-08 21:51:09 -0700

    longest sequence

--- 15-hash-tables-exercise/exercise.py
+++ 15-hash-tables-exercise/exercise.py
@@ -120,3 +120,20 @@ def find_pairs(arr1, arr2, target):
         if compliment in set1:
             pairs.append((compliment, num))
     return pairs
+
+def longest_consecutive_sequence(nums):
+    nums_set = set(nums)
+    longest_sequence = 0
+
+    for num in nums:
+        if num - 1 not in nums_set:
+            current_num = num
+            current_sequence = 1
+
+            while current_num + 1 in nums_set:
+                current_num += 1
+                current_sequence += 1
+
+            longest_sequence = max(longest_sequence, current_sequence)
+
+    return longest_sequence

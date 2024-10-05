81abbe41b6505979c438824225eb26ad1d679d43
mykkelol <1micheal.sihavong2@gmail.com>
2024-10-05 10:06:08 -0700

    dry sink down

--- 19-heap-excercise/exercise.py
+++ 19-heap-excercise/exercise.py
@@ -75,10 +75,14 @@ class MinHeap:
             left_index = self._left_child(index)
             right_index = self._right_child(index)
 
-            if left_index < len(self.heap) and self.heap[left_index] < self.heap[min_index]:
+            heap_len = len(self.heap)
+            left_value = self.heap[left_index]
+            right_value = self.heap[right_index]
+
+            if left_index < heap_len and left_value < self.heap[min_index]:
                 min_index = left_index
             
-            if right_index < len(self.heap) and self.heap[right_index] < self.heap[min_index]:
+            if right_index < heap_len and right_value < self.heap[min_index]:
                 min_index = right_index
 
             if min_index is not index:

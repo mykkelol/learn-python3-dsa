30feab395458140f73212045c713e1073dab6f28
mykkelol <1micheal.sihavong2@gmail.com>
2024-10-08 11:51:01 -0700

    add max heap for lc

--- 20-heap-lc/excercise.py
+++ 20-heap-lc/excercise.py
@@ -1,3 +1,35 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+    
+    def _right_child(self, index):
+        return 2 * index + 2
+    
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+            heap_size = len(self.heap)
+
+            if left_index < heap_size and self.heap[left_index] > self.heap[max_index]:
+                max_index = left_index
+            
+            if right_index < heap_size and self.heap[right_index] > self.heap[max_index]:
+                max_index = right_index
+
+            if max_index != index:
+                self.heap[index], self.heap[max_index] = self.heap[max_index], self.heap[index]
+                index = max_index
+            else:
+                return
+    
 def find_kth_smallest(nums, k):
     max_heap = MaxHeap()
 

bd3454757e98498d8187a7815519bae6b8593038
mykkelol <1micheal.sihavong2@gmail.com>
2024-10-09 11:28:27 -0700

    add insert

--- 20-heap-lc/excercise.py
+++ 20-heap-lc/excercise.py
@@ -32,6 +32,14 @@ class MaxHeap:
                 index = max_index
             else:
                 return
+            
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+    
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
     
 def find_kth_smallest(nums, k):
     max_heap = MaxHeap()

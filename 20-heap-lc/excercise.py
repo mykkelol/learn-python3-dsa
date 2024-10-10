c1102246553a394277b17280a6b6ecc75d291069
mykkelol <1micheal.sihavong2@gmail.com>
2024-10-10 15:21:08 -0700

    add remove

--- 20-heap-lc/excercise.py
+++ 20-heap-lc/excercise.py
@@ -40,6 +40,18 @@ class MaxHeap:
         while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        return max_value
     
 def find_kth_smallest(nums, k):
     max_heap = MaxHeap()
